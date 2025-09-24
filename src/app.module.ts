e7754e420f3f560bacc76d0112d709d3208e5920
Tuandev18 <phamtuan180704@gmail.com>
2025-09-24 16:12:18 +0700

    create module for project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { InterviewModule } from './interview/interview.module';
+import { QuestionModule } from './question/question.module';
+import { ResponseModule } from './response/response.module';
+import { ReportModule } from './report/report.module';
+import { BlogModule } from './blog/blog.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    InterviewModule,
+    QuestionModule,
+    ResponseModule,
+    ReportModule,
+    BlogModule,
+    AdminModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
